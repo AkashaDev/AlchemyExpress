e9a34f5251ecb74de1a291eab732f885236bba8c
La Vito Valentino M <lavito.val11@gmail.com>
2025-07-31 20:51:32 +0700

    Audio Manager Update
    Add Sound and Music Library
    add Sound and Music Manager for UI Menu

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -2,16 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+    public Slider musicSlider;
+    public Slider sfxSlider;
     private void Start()
     {
-        
+        LoadVolume();
+        MusicManager.Instance.PlayMusic("MainMenu");
     }
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
+        MusicManager.Instance.PlayMusic("Game");
         
     }
     
@@ -19,5 +26,30 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("game sudah diexit!");
+    }
+
+    public void UpdateMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+
+    public void UpdateSoundVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", volume);
+    }
+
+    public void SaveVolume()
+    {
+        audioMixer.GetFloat("MusicVolume", out float musicVolume);
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+
+        audioMixer.GetFloat("SFXVolume", out float sfxVolume);
+        PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
+    }
+
+    public void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume");
     }
 }
