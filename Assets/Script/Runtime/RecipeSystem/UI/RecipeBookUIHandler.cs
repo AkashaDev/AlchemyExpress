958d724f52d91bc680b4b95ff843e4d745f399c2
Ragasa26 <ragasa2609@webmail.umm.ac.id>
2026-08-24 08:24:15 +0700

    Reapply "Merge remote-tracking branch 'origin/15-integrasi-aset' into 16-merger-assets-and-inventory"
    This reverts commit cf1938b3d215469a876070b62028f36429d24280.

--- Assets/Script/Runtime/RecipeSystem/UI/RecipeBookUIHandler.cs
+++ Assets/Script/Runtime/RecipeSystem/UI/RecipeBookUIHandler.cs
@@ -5,8 +5,7 @@ using System.Collections.Generic;
 using TMPro;
 
 public class RecipeBookUIHandler : MonoBehaviour
-{   
-    
+{
     [Header("UI References")]
     public GameObject recipeBookPanel;
     public GameObject recipeDetailPanel;
@@ -60,6 +59,7 @@ public class RecipeBookUIHandler : MonoBehaviour
         recipeBookPanel.SetActive(!recipeBookPanel.activeSelf);
         if (recipeBookPanel.activeSelf && allPotions.Count > 0)
         {
+            currentPotionIndex = 0;
             // Langsung tampilkan tanpa transisi saat pertama kali dibuka
             ShowRecipeDetails(allPotions[currentPotionIndex]);
             recipeDetailCanvasGroup.alpha = 1; // Pastikan terlihat jelas
